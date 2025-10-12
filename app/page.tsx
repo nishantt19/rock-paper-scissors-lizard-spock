f652843bab53964a007a80b5a7e5024298f27b77
Nishant <nyd44533@gmail.com>
2025-10-12 10:36:48 +0530

    chore: initial project setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center min-h-screen">
+      <h1 className="text-4xl font-bold">
+        ROCK, PAPER, SCISSORS, LIZARD, SPOCK
+      </h1>
+    </main>
+  );
+}
